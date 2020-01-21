cb13cf0771a2b82d27d0a07492c5a81f0202ff2f
Tao Wang <hellotaotao@gmail.com>
2020-01-21 13:31:00 +1030

    Update launch date again. And make sure only check launch date when the plugin is set to NZ

--- app/code/community/Humm/HummPayments/Block/Form/HummPayments.php
+++ app/code/community/Humm/HummPayments/Block/Form/HummPayments.php
@@ -8,11 +8,14 @@
  */
 class Humm_HummPayments_Block_Form_HummPayments extends Mage_Payment_Block_Form {
     const LAUNCH_TIME_URL = "https://humm-variables.s3-ap-southeast-2.amazonaws.com/nz-launch-time.txt";
-    const LAUNCH_TIME_DEFAULT = "2020-02-23 13:30:00 UTC";
-    const LAUNCH_TIME_CHECK_ENDS = "2020-05-23 13:30:00 UTC";
+    const LAUNCH_TIME_DEFAULT = "2020-04-30 14:30:00 UTC";
+    const LAUNCH_TIME_CHECK_ENDS = "2020-07-30 14:30:00 UTC";
 
     protected function _construct() {
-        $this->updateLaunchDate();
+        $country = Mage::getStoreConfig( 'payment/HummPayments/specificcountry' );
+        if ( $country == 'NZ' ) {
+            $this->updateLaunchDate();
+        }
         $mark = Mage::getConfig()->getBlockClassName( 'core/template' );
         $mark = new $mark;
         $mark->setTemplate( 'HummPayments/mark.phtml' );
