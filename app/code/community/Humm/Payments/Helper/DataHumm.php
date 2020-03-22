71dce4cc950d4f5ad255d84dffc59d08fad5252f
Roger Bi <roger.bi@flexigroup.com.au>
2020-03-23 09:28:41 +1100

    Increase NZ launch_time_check_ends

--- app/code/community/Humm/Payments/Helper/DataHumm.php
+++ app/code/community/Humm/Payments/Helper/DataHumm.php
@@ -10,7 +10,7 @@ class Humm_Payments_Helper_DataHumm extends Mage_Core_Helper_Abstract
 
     const LAUNCH_TIME_URL = 'https://humm-variables.s3-ap-southeast-2.amazonaws.com/nz-launch-time.txt';
     const LAUNCH_TIME_DEFAULT = "2020-05-11 00:00:00 UTC";
-    const LAUNCH_TIME_CHECK_ENDS = "2020-05-18 00:00:00 UTC";
+    const LAUNCH_TIME_CHECK_ENDS = "2020-11-18 00:00:00 UTC";
     const Log_file = 'humm.log';
     const URLS = [
         'AU' => [
